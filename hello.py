a2b22286204d763c4235f8951d1ac4dee71b448e
Andrés I <suurrend3r@gmail.com>
2019-04-01 13:09:38 -0300

    Cambio nombre de dominio·

---
+++ hello.py
@@ -0,0 +1,12 @@
+class Prueba(object):
+  def __init__(self,M,I):
+    self.M = M
+    self.I = I
+  def __repr__(self):
+    print("Esto es una prueba.")
+
+def main():
+  print("2018")
+
+def __name__ == "__main__":
+  main()
